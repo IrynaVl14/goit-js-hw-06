761fe9fc9096beb001b41347d04e27b6da44887e
Iryna Vlasenko <via-lilly@ukr.net>
2021-11-09 22:29:13 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector('.login-form');
+const inputMail = form.querySelector('[type="email"]');
+const inputPassword = form.querySelector('[type="password"]');
 
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    onFormValidate();
+    onFormData();    
+    form.reset();
+}
+
+function onFormValidate() {    
+    if (inputPassword.value === '' || inputMail.value === '' ) {
+       return alert('Все поля должны быть заполнены');
+        
+    }
+       return;       
+}
+
+function onFormData() {
+    const formElement = event.currentTarget.elements;   
+    const mail = formElement.email.value;
+    const password = formElement.password.value;
+    const formData = {
+        mail, password
+    };
+    console.log(formData);
+}
