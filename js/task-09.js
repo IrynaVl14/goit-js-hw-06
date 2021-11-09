06571d8a873488888d0ce02f01c1a0c0072a1ba0
Iryna Vlasenko <via-lilly@ukr.net>
2021-11-09 22:53:12 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const widgetEl = document.querySelector('.widget');
+const textEl = document.querySelector('span.color');
+
+widgetEl.addEventListener('click', onColorChangeClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onColorChangeClick() {  
+  textEl.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = textEl.textContent;
 }
