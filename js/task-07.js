90878303b917f68bf56c2173d27f4b2008853ce4
Iryna Vlasenko <via-lilly@ukr.net>
2021-11-09 19:32:37 +0200

    task7

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector('input#font-size-control');
+const textEl = document.querySelector('span#text');
+
+inputEl.addEventListener('input', onInputChange)
+
+function onInputChange() {   
+    textEl.style.fontSize = inputEl.value+'px';
+}
