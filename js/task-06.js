e0fe9597267cb158f6730113af2f69b56fee4d2f
Iryna Vlasenko <via-lilly@ukr.net>
2021-11-12 12:47:34 +0200

    update task6

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,13 @@ const inputEl = document.querySelector('input#validation-input');
 inputEl.addEventListener('blur', onInputBlur)
 
 function onInputBlur(event) {
-
-    if (event.currentTarget.value.length<=inputEl.dataset.length) {
-        return inputEl.classList.add('valid')
-    }
     inputEl.classList.add('invalid')
+    const inputLength = event.currentTarget.value.length;
+    const dataLength = Number(inputEl.dataset.length);
+    if (inputLength === dataLength) {
+     inputEl.classList.remove('invalid')
+     inputEl.classList.add('valid')
+    }
+   
 }
 
